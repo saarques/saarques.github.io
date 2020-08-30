06648f36868c40bfea113fce25d84e03158c679e
saarques <saraswatgajendra97@gmail.com>
2020-08-30 21:14:25 +0530

    commented unnecessary part

--- service-worker.js
+++ service-worker.js
@@ -3,20 +3,20 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-// set names for both precache & runtime cache
-workbox.core.setCacheNameDetails({
-  prefix: "my-blog",
-  suffix: "v1",
-  precache: "precache",
-  runtime: "runtime-cache",
-});
+// // set names for both precache & runtime cache
+// workbox.core.setCacheNameDetails({
+//   prefix: "my-blog",
+//   suffix: "v1",
+//   precache: "precache",
+//   runtime: "runtime-cache",
+// });
 
-// let Service Worker take control of pages ASAP
-workbox.skipWaiting();
-workbox.clientsClaim();
+// // let Service Worker take control of pages ASAP
+// workbox.skipWaiting();
+// workbox.clientsClaim();
 
-// let Workbox handle our precache list
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+// // let Workbox handle our precache list
+// workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 if (workbox) {
   workbox.routing.registerRoute(
