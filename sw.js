0cc4a7bfcf7fac0f6ae3cfba1e653a0d09b41974
saarques <saraswatgajendra97@gmail.com>
2020-08-30 18:35:06 +0530

    added workbox, updated head

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 // service-worker.js
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
 
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
-    prefix: 'my-blog',
-    suffix: 'v1',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+  prefix: "my-blog",
+  suffix: "v1",
+  precache: "precache",
+  runtime: "runtime-cache",
 });
 
 // let Service Worker take control of pages ASAP
@@ -15,20 +18,54 @@ workbox.clientsClaim();
 // let Workbox handle our precache list
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-// use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    workbox.strategies.networkFirst()
-);
+if (workbox) {
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "static-resources",
+    })
+  );
 
-// use `cacheFirst` strategy for images
-workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    workbox.strategies.cacheFirst()
-);
+  workbox.routing.registerRoute(
+    // Cache image files.
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    // Use the cache if it's available.
+    new workbox.strategies.CacheFirst({
+      // Use a custom cache name.
+      cacheName: "image-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images.
+          maxEntries: 20,
+          // Cache for a maximum of a week.
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        }),
+      ],
+    })
+  );
 
-// third party files
-// workbox.routing.registerRoute(
-//     /^https?:\/\/cdn.staticfile.org/,
-//     workbox.strategies.staleWhileRevalidate()
-// );
+  // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: "google-fonts-stylesheets",
+    })
+  );
+
+  // Cache the underlying font files with a cache-first strategy for 1 year.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+      cacheName: "google-fonts-webfonts",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    })
+  );
+}
